05b81b9c3f58eaafb05a1caa05eadfd73f848451
Chao Sun <sunchao@apache.org>
2017-12-02 23:22:19 -0800

    Update VIM configs

---
+++ .vim/update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+repos=(
+  itchyny/lightline.vim
+  junegunn/fzf.vim
+  mileszs/ack.vim
+  tomasr/molokai
+  tpope/vim-pathogen
+  tpope/vim-fugitive
+  scrooloose/nerdtree
+)
+
+set -e
+DIR=$(cd $(dirname $0); pwd -P)/bundle
+echo "DIR: $DIR"
+
+if [ -d $DIR -a -z "$1" ]; then
+  temp=$(mktemp -d -t bundleXXXXX)
+  echo "Moving old bundle dir to $temp"
+  mv "$DIR" "$temp"
+fi
+mkdir $DIR
+
+for repo in ${repos[@]}; do
+  if [ -n "$1" ]; then
+    if ! (echo $repo | grep -i $i &> /dev/null); then
+      continue
+    fi
+  fi
+  plugin=$(basename $repo | sed -e 's/\.git$//')
+  dst="$DIR/$plugin"
+  rm -rf $dst
+  (
+    git clone --depth=1 -q https://github.com/$repo $dst
+    rm -rf $dst/.git
+    echo "Cloned $repo"
+  ) &
+done
+wait
